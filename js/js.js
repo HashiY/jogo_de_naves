9e7caea258a406c9f9e61aebf562acfcc9584d95
VSF <47865897+HashiY@users.noreply.github.com>
2022-01-25 18:11:18 -0300

    Parte 4

---
+++ js/js.js
@@ -0,0 +1,8 @@
+function start() {
+	$("#inicio").hide();
+	
+	$("#fundoGame").append("<div id='jogador'></div>");//criando essas div no fundogame
+	$("#fundoGame").append("<div id='inimigo1'></div>");
+	$("#fundoGame").append("<div id='inimigo2'></div>");
+	$("#fundoGame").append("<div id='amigo'></div>");
+}
